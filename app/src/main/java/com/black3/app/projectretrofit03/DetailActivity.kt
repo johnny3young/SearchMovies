0a19c22695ed9e22e3dcbe66ec20953e66886b3b
johnny3young <youngerblack@gmail.com>
2019-07-19 11:06:07 -0500

    Expose the URL STRING in constant variables

--- app/src/main/java/com/black3/app/projectretrofit03/DetailActivity.kt
+++ app/src/main/java/com/black3/app/projectretrofit03/DetailActivity.kt
@@ -2,21 +2,21 @@ package com.black3.app.projectretrofit03
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
 import com.squareup.picasso.Picasso
-import com.squareup.picasso.RequestCreator
 import kotlinx.android.synthetic.main.item_movie_details.*
 
 class DetailActivity : AppCompatActivity() {
-
+    
+    val PATH_URL_IMAGE= "https://image.tmdb.org/t/p/w500/"
+    
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.item_movie_details)
 
-        mostrarDetallesMovies()
+        ShowDetailsMovies()
     }
 
-    private fun mostrarDetallesMovies()  {
+    private fun ShowDetailsMovies()  {
         val bundle = intent.extras
 
         val popularity = bundle.get("popularity")
@@ -26,7 +26,7 @@ class DetailActivity : AppCompatActivity() {
         textViewVote_count.text = votecount.toString()
 
         val posterpath = bundle.get("posterpath")
-        Picasso.get().load("https://image.tmdb.org/t/p/w500/$posterpath").into(ivMovieDetails)
+        Picasso.get().load(PATH_URL_IMAGE +"$posterpath").into(ivMovieDetails)
 
         val overview = bundle.get("overview")
         textViewOverview.text = overview.toString()
